32a7f4174c7c95cc1493c8bba725180ba85fe6b0
surajsly <surajsingh9810859209@gmail.com>
2021-07-08 15:41:56 +0530

    Lower Upper

---
+++ C++ Fundamentals September 2021/013LowerUpper.cpp
@@ -0,0 +1,46 @@
+// 013LowerUpper.cpp
+
+/*
+Print "lowercase" if user enters a character between 'a-z' , Print "UPPERCASE" is character lies between 'A-Z' and print "Invalid" for all other characters like $,.^# etc.
+
+Input Format
+Single Character .
+
+Constraints
+-
+
+Output Format
+lowercase UPPERCASE Invalid
+
+Sample Input
+$
+
+Sample Output
+Invalid
+
+Explanation
+-
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    if (ch >= 'A' and ch <= 'Z')
+    {
+        cout << "UPPERCASE";
+    }
+    else if (ch >= 'a' and ch <= 'z')
+    {
+        cout << "lowercase";
+    }
+    else
+    {
+        cout << "Invalid";
+    }
+
+    return 0;
+}
